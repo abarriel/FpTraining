565a727523f5cc2ed0e347de2718ba8e3b31fb79
abarriel <abarriel@student.42.fr>
2017-08-10 13:02:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+const R = require('ramda');
+// let data1 = [1, 2, 3, 4];
+// let add = a => b => a + b;
+// let incr = add(1);
+// console.log(data1.map(incr));
+// let add1 = R.add(1);
+// incr = R.map(add1);
+// console.log(incr(data1));
+
+
+// const add = (a, b) => a + b;
+// console.log(data1.reduce(add, 1));
+// let fact =  R.reduce(R.multiply, 1);
+// // console.log(fact(data1));
+
+// let range = size => Array.from(new Array(size), (x, i) => i + 1);
+// fact = size => R.reduce(R.multiply, 1, range(size));
+// console.log(fact(5));
+// // console.log(range(10));
+
+// data1 = [
+//     { id: 1, label: 'data1' },
+//     { id: 2, label: 'data2' },
+//     { id: 3, label: 'data3' },
+//     { id: 4, label: 'data4' },
+// ];
+
+// console.log(data1.reduce((acc, items) => ({ ...acc, [items.id]: items }), {}));
+// const toDict = R.reduce((acc, v) => ({...acc, [v.id]: v }), {});
+// console.log(data1.reduce((acc, items) => Object.assign({}, acc, { [items.id]: items}), {}));
+
+// const toPairs = R.map(x => [x.id, x])
+
+// const fromPairs = R.reduce((acc, [id, v]) => ({ ...acc, [id]: v }), {});
+// const makeDict = R.compose(R.fromPairs, R.toPairs);
+// console.log(makeDict(data1));
+// console.log(R.fromPairs(R.toPairs(data1)));
+
+// map = reduce et concat;
+
+// const map = R.reduce((acc, v) => R.concat(acc, [v]), []);
+
+// console.log(R.filter((x) => x % 2, data1));
+// const mapper = R.compose(mapper, R.concat);
+// // const mapper = R.compose(mapper, R.concat);
+// console.log(map(data1));
+
+// const isOdd = x => x % 2;
+
+// let data = [1, 2 ,3 ,4 ,5 ,6];
+// // const filterOddNumbers = R.filter(isOdd);
+// // data = R.times((n) => n + 1, 5);
+// // console.log(data);
+// // // console.log(filterOddNumbers(data));
+// // const add =
+// // const map = R.reduce((acc, v) => isOdd(v), []);
+// console.log(map(data));
+const X = 1;
+const O = 2;
+
+let board = [
+  O, X, O,
+    ,O, X,
+  X,X, X
+];
+
+const oneLine = (tab, line) => tab.filter((value, index) => parseInt(index / 3) === line);
+
+const oneColumn = (tab, line) => tab.filter((value, index) => index % 3 === line);
+
+const oneDiagonalLeft = (tab, line) => tab.filter((value, index) => parseInt(index / 3) === (index % 3));
+
+const oneDiagonalRight = (tab, line) => tab.filter((value, index) => (parseInt(index / 3) + (index % 3) === line));
+
+const isFull = (line, lineFilter) => (tab) => {
+   const res = R.countBy(R.identity)(lineFilter(tab, line));
+   return (res[X] === 3 || res[O] === 3);
+};
+
+// 0 1 2 
+// 3 4 5
+// 6 7 8
+
+// MATRIX 
+// 0,0 0,1 0,2
+// 1,0 1,1 1,2
+// 2,0 2,1 2,2
+
+const patterns = [
+  isFull(0, oneLine), 
+  isFull(1, oneLine), 
+  isFull(2, oneLine), 
+  isFull(0, oneColumn),
+  isFull(1, oneColumn),
+  isFull(2, oneColumn),
+  isFull(0, oneDiagonalLeft),
+  isFull(2, oneDiagonalRight),
+];
+const isEndend = board =>  patterns.some(fct => fct(board));
+console.log('Should be true', isEndend(board));
+board = [
+  O, X, O,
+  O, O ,O,
+  X, X, ,
+]
+console.log('Should be true', isEndend(board));
+board = [
+  O, X, O,
+  , O ,O,
+  X, X, ,
+]
+console.log('Should be false', isEndend(board));
+board = [
+  O, X, O,
+  ,  ,,
+  X, X, ,
+]
+console.log('Should be false', isEndend(board));
+board = [
+  O, X, O,
+  ,  X, ,
+  X, X, ,
+]
+console.log('Should be true', isEndend(board));
+board = [
+  , , ,
+  ,  , ,
+  , , ,
+]
+console.log('Should be false', isEndend(board));
+board = [
+  X, X, O,
+  ,  X, ,
+  , , X,
+]
+console.log('Should be true', isEndend(board));
---
+++ todo.js
@@ -0,0 +1,139 @@
+const R = require('ramda');
+
+const todos = [
+  {
+    id: 1,
+    label: 'Learning JS stack',
+    type: 'learning',
+    tasks: [
+      {
+        id: 1,
+        description: 'Learn Promises',
+        isDone: false,
+        runningTime: 24,
+      },
+      {
+        id: 2,
+        description: 'Learn Async.js',
+        isDone: true,
+        runningTime: 45,
+      },
+      {
+        id: 3,
+        description: 'Learn Ramda.js',
+        isDone: false,
+        runningTime: 13,
+      },
+    ]
+  },
+  {
+    id: 2,
+    label: 'Learning how to craft a SPA',
+    dueDate: new Date(2017, 4, 22),
+    type: 'learning',
+    tasks: [
+      {
+        id: 1,
+        description: 'Learn React',
+        isDone: true,
+        runningTime: 22,
+      },
+      {
+        id: 2,
+        description: 'Learn Redux',
+        isDone: false,
+        runningTime: 7,
+      },
+    ]
+  },
+  {
+    id: 3,
+    label: 'code first sample',
+    dueDate: new Date(2017, 4, 22),
+    type: 'coding',
+  },
+  {
+    id: 4,
+    label: 'implement tests',
+    dueDate: new Date(2019, 12, 11),
+    type: 'coding',
+  },
+];
+
+// 1
+// console.log('Mapper 1');
+// const mapper = R.compose(
+//   R.values,
+//   R.pick([ 'type', 'label', 'dueDate']),
+// )
+// const arrayTodos = R.map((value, index) => mapper(value), todos);
+// console.log(arrayTodos);
+// console.log('\n');
+
+// // 1
+// const getValues = R.map(R.values);
+// const pickValues = R.map(R.pick([ 'type', 'label', 'dueDate']));
+// const mapper2 = R.compose(getValues, pickValues)
+// console.log(mapper2(todos));
+// console.log('\n');
+
+// const handler = R.compose(
+//   R.values,
+//   R.pick([ 'type', 'label', 'dueDate']),
+// )
+// const mapperr = R.compose(
+//   R.map(R.values),
+//   R.map(handler),
+// )
+// console.log(mapper2(todos));
+// console.log('\n');
+
+
+// // test 2
+// const incomingMessage = ({ dueDate }) => dueDate > Date.now();
+
+// const mapper3 = R.compose(
+//   mapper2,
+//   R.filter(incomingMessage),
+// )
+
+// console.log(mapper3(todos));
+// console.log('\n');
+//  // test 3
+const isLearning = ({ type }) => type === 'learning';
+// const teste = (acc, v, array) => {
+//   console.log('ok');
+//   console.log(acc);
+//   console.log(v);
+//   console.log(array);
+//   return acc + v;
+// };
+
+// console.log('Mapper 3');
+// const mapper4 = R.compose(
+//   R.reduce(R.add, 0),
+//   R.map(R.propOr(0, 'runningTime')),
+//   R.reduce(R.concat, []),
+//   R.map(R.propOr(0, 'tasks')),
+//   R.filter(isLearning),
+// )
+
+// console.log(mapper4(todos));
+// console.log('\n');
+// // test4
+
+// // console.log(todos);
+// console.log('\n');
+// console.log('Mapper 4');
+// const test = (a, b, c) => {
+//   return ;
+// };
+const filterBy = (filter) => R.filter(filter);
+const pluckBy = (whichItem) => R.pluck(whichItem);
+const mapper5 = R.compose(
+  R.chain(pluckBy('runningTime')),
+  pluckBy('tasks'),
+  filterBy(isLearning),
+)
+console.log('\n');
+console.log(mapper5(todos));
